0762800348038c6117f9b5a202436e62a643e4fc
Good-Boys-Exe <87267285+Good-Boys-Exe@users.noreply.github.com>
2021-07-25 11:35:47 +0700

    Update song.py

--- GohanMusic/song.py
+++ GohanMusic/song.py
@@ -51,9 +51,11 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"""**🏷 Nama Lagu:** [{title}]({link})
+        rep = f"""
+**🏷 Nama Lagu:** [{title}]({link})
 **🎵 Diunggah Oleh;** [{BOT_NAME}](https://t.me/{BOT_USERNAME})
-**🎧 Permintaan Dari:** {rpk}"""
+**🎧 Permintaan Dari:** {rpk}
+"""
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
