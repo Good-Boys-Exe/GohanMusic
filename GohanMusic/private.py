14e478477e19ec64e49d4891b339232ead142acc
Good-Boys-Exe <87267285+Good-Boys-Exe@users.noreply.github.com>
2021-07-25 08:37:24 +0700

    Create private.py

---
+++ GohanMusic/private.py
@@ -0,0 +1,172 @@
+from time import time
+from datetime import datetime
+from config import BOT_USERNAME, BOT_NAME, ASSISTANT_NAME, SUPPORT_GROUP, STICKER_ID, OWNER
+from helpers.filters import command
+from pyrogram import Client, filters, emoji
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from helpers.decorators import authorized_users_only
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+
+
+@Client.on_message(command("start") & filters.private & ~filters.edited)
+async def start_(client: Client, message: Message):
+    await message.reply_sticker(f"{STICKER_ID}")
+    await message.reply_text(
+        f"""<b>Hallo {message.from_user.mention} Nama saya [{BOT_NAME}](https://t.me/{BOT_USERNAME})\n
+Saya Adalah Bot Canggih Yang Dibuat Untuk Memutar Musik Di Obrolan Suara Grup Telegram\n
+Ketik » /help « Untuk Melihat Daftar Perintah Saya!
+</b>""",
+        reply_markup=InlineKeyboardMarkup(
+            [ 
+                [
+                    InlineKeyboardButton(
+                        "➕ ᴛᴀᴍʙᴀʜᴋᴀɴ ᴋᴇ ɢʀᴏᴜᴘ ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+                  ],[
+                    InlineKeyboardButton(
+                         "🙎🏻‍♂ ᴀssɪsᴛᴀɴᴛ", url=f"https://t.me/{ASSISTANT_NAME}"
+                    ),
+                    InlineKeyboardButton(
+                        "ᴏᴡɴᴇʀ 🧑🏻‍💻", url=f"https://t.me/{OWNER}"
+                    )
+                ]
+            ]
+        ),
+     disable_web_page_preview=False
+    )
+
+
+@Client.on_message(command(["start", f"start@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
+async def start(client: Client, message: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await message.reply_text(
+        f"""Saya Sedang Online!\n<b>Waktu Online:</b> `{uptime}`""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🧑🏻‍💻 ᴏᴡɴᴇʀ", url=f"https://t.me/{OWNER}"
+                    ),
+                    InlineKeyboardButton(
+                        "ɢʀᴏᴜᴘ 💬", url=f"https://t.me/{SUPPORT_GROUP}"
+                    )
+                ]
+            ]
+        )
+    )
+
+
+@Client.on_message(command("help") & filters.private & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_text(
+        f"""<b>Hallo {message.from_user.mention}
+\n**Untuk Semua**
+/play (judul lagu) - Untuk Memutar lagu yang Anda minta melalui YouTube
+/playlist - Untuk Menampilkan daftar putar Lagu sekarang
+/current - Untuk Menunjukkan  Lagu sekarang yang sedang diputar
+/song (judul lagu) - Untuk Mendownload lagu dari YouTube 
+/search (judul video) - Untuk Mencari Video di YouTube dengan detail
+/video (judul video) - Untuk Mendownload Video di YouTube dengan detail
+\n**Hanya Admin:**
+/player - Buka panel pengaturan pemutar musik
+/pause - Untuk Menjeda pemutaran Lagu
+/resume - Untuk Melanjutkan pemutaran Lagu yang di pause
+/skip - Untuk Menskip pemutaran lagu ke Lagu berikutnya
+/end - Untuk Memberhentikan pemutaran Lagu
+/userbotjoin - Untuk Mengundang asisten ke obrolan Anda
+/reload - Untuk Merefresh admin list
+</b>""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "💬 ɢʀᴏᴜᴘ", url=f"https://t.me/{SUPPORT_GROUP}"
+                    ),
+                    InlineKeyboardButton(
+                        "ᴏᴡɴᴇʀ 🧑🏻‍💻", url=f"https://t.me/{OWNER}"
+                    )
+                ]
+            ]
+        )
+    )
+
+
+@Client.on_message(command(["help", f"help@{BOT_USERNAME}"]) & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_text(
+        f"""<b>Hallo {message.from_user.mention}
+\n**Untuk Semua**
+/play (judul lagu) - Untuk Memutar lagu yang Anda minta melalui YouTube
+/playlist - Untuk Menampilkan daftar putar Lagu sekarang
+/current - Untuk Menunjukkan  Lagu sekarang yang sedang diputar
+/song (judul lagu) - Untuk Mendownload lagu dari YouTube 
+/search (judul video) - Untuk Mencari Video di YouTube dengan detail
+/video (judul video) - Untuk Mendownload Video di YouTube dengan detail
+\n**Hanya Admin:**
+/player - Buka panel pengaturan pemutar musik
+/pause - Untuk Menjeda pemutaran Lagu
+/resume - Untuk Melanjutkan pemutaran Lagu yang di pause
+/skip - Untuk Menskip pemutaran lagu ke Lagu berikutnya
+/end - Untuk Memberhentikan pemutaran Lagu
+/userbotjoin - Untuk Mengundang asisten ke obrolan Anda
+/reload - Untuk Merefresh admin list
+</b>""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "💬 ɢʀᴏᴜᴘ", url=f"https://t.me/{SUPPORT_GROUP}"
+                    ),
+                    InlineKeyboardButton(
+                        "ᴏᴡɴᴇʀ 🧑🏻‍💻", url=f"https://t.me/{OWNER}"
+                    )
+                ]
+            ]
+        )
+    )
+
+
+@Client.on_message(command(["ping", f"ping@{BOT_USERNAME}"]) & ~filters.edited)
+async def ping_pong(client: Client, m: Message):
+    start = time()
+    m_reply = await m.reply_text("Pinging...")
+    delta_ping = time() - start
+    await m_reply.edit_text(
+        f"{emoji.PING_PONG} **PONG!!**\n"
+        f"`{delta_ping * 1000:.3f} ms`"
+    )
+
+
+@Client.on_message(command(["uptime", f"uptime@{BOT_USERNAME}"]) & ~filters.edited)
+@authorized_users_only
+async def get_uptime(client: Client, m: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await m.reply_text(
+        f"{emoji.ROBOT} Saya Masih Aktif\n"
+        f"• **Waktu aktif:** `{uptime}`\n"
+        f"• **Waktu mulai:** `{START_TIME_ISO}`"
+    )
