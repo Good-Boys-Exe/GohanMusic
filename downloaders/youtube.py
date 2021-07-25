acfd9ec3782794964e6e6323be957d2b3dfe3a95
Good-Boys-Exe <87267285+Good-Boys-Exe@users.noreply.github.com>
2021-07-25 08:51:39 +0700

    Create youtube.py

---
+++ downloaders/youtube.py
@@ -0,0 +1,31 @@
+from os import path
+
+from youtube_dl import YoutubeDL
+
+from config import BOT_NAME as bn, DURATION_LIMIT
+from helpers.errors import DurationLimitError
+ydl_opts = {
+    "format": "bestaudio[ext=m4a]",
+    "geo-bypass": True,
+    "nocheckcertificate": True,
+    "outtmpl": "downloads/%(id)s.%(ext)s",
+}
+
+ydl = YoutubeDL(ydl_opts)
+
+
+def download(url: str) -> str:
+    info = ydl.extract_info(url, False)
+    duration = round(info["duration"] / 60)
+
+    if duration > DURATION_LIMIT:
+        raise DurationLimitError(
+            f"❌ Video lebih panjang dari {DURATION_LIMIT} menit tidak diperbolehkan, video yang disediakan adalah {duration} menit)"
+        )
+    try:
+        ydl.download([url])
+    except:
+        raise DurationLimitError(
+            f"❌ Video lebih panjang dari {DURATION_LIMIT} menit tidak diperbolehkan, video yang disediakan adalah {duration} menit)"
+        )
+    return path.join("downloads", f"{info['id']}.{info['ext']}")
