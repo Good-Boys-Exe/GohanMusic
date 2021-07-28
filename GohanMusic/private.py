14f5490a358590b22526fcbc3469cc572e76531b
Good-Boys-Exe <87267285+Good-Boys-Exe@users.noreply.github.com>
2021-07-28 19:04:45 +0700

    Update private.py

--- GohanMusic/private.py
+++ GohanMusic/private.py
@@ -7,28 +7,6 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from helpers.decorators import authorized_users_only
 
 
-START_TIME = datetime.utcnow()
-START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
-TIME_DURATION_UNITS = (
-    ('week', 60 * 60 * 24 * 7),
-    ('day', 60 * 60 * 24),
-    ('hour', 60 * 60),
-    ('min', 60),
-    ('sec', 1)
-)
-
-async def _human_time_duration(seconds):
-    if seconds == 0:
-        return 'inf'
-    parts = []
-    for unit, div in TIME_DURATION_UNITS:
-        amount, seconds = divmod(int(seconds), div)
-        if amount > 0:
-            parts.append('{} {}{}'
-                         .format(amount, unit, "" if amount == 1 else "s"))
-    return ', '.join(parts)
-
-
 @Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
@@ -58,9 +36,6 @@ Ketik » /help « Untuk Melihat Daftar Perintah Saya!
 
 @Client.on_message(command(["start", f"start@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
 async def start(client: Client, message: Message):
-    current_time = datetime.utcnow()
-    uptime_sec = (current_time - START_TIME).total_seconds()
-    uptime = await _human_time_duration(int(uptime_sec))
     await message.reply_text(
         f"""Saya Sedang Online!\n<b>Waktu Online:</b> `{uptime}`""",
         reply_markup=InlineKeyboardMarkup(
