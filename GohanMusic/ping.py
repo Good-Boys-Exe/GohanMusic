78d308b1c6b04ce6829a52079624cc9625e85588
Good-Boys-Exe <87267285+Good-Boys-Exe@users.noreply.github.com>
2021-07-29 06:23:00 +0700

    Create ping.py

---
+++ GohanMusic/ping.py
@@ -0,0 +1,75 @@
+from time import time
+from datetime import datetime
+from config import BOT_USERNAME, BOT_NAME, ASSISTANT_NAME, SUPPORT_GROUP, STICKER_ID, OWNER
+from helpers.filters import command
+from pyrogram import Client, filters, emoji
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from helpers.decorators import authorized_users_only
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+
+
+@Client.on_message(command(["start", f"start@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
+async def start(client: Client, message: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await message.reply_text(
+        f"""Saya Sedang Online!\n<b>Waktu Online:</b> `{uptime}`""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🧑🏻‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ", url=f"https://t.me/{OWNER}"
+                    ),
+                    InlineKeyboardButton(
+                        "sᴜᴘᴘᴏʀᴛ 💬", url=f"https://t.me/{SUPPORT_GROUP}"
+                    )
+                ]
+            ]
+        )
+    )
+
+
+@Client.on_message(command(["ping", f"ping@{BOT_USERNAME}"]) & ~filters.edited)
+async def ping_pong(client: Client, m: Message):
+    start = time()
+    m_reply = await m.reply_text("Pinging...")
+    delta_ping = time() - start
+    await m_reply.edit_text(
+        f"{emoji.PING_PONG} **PONG!!**\n"
+        f"`{delta_ping * 1000:.3f} ms`"
+    )
+
+
+@Client.on_message(command(["uptime", f"uptime@{BOT_USERNAME}"]) & ~filters.edited)
+@authorized_users_only
+async def get_uptime(client: Client, m: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await m.reply_text(
+        f"{emoji.ROBOT} Saya Masih Aktif\n"
+        f"• **Waktu aktif:** `{uptime}`\n"
+        f"• **Waktu mulai:** `{START_TIME_ISO}`"
+    )
