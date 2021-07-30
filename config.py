ba277336269d01c1573c3e40f40a416d618f3556
Good-Boys-Exe <87267285+Good-Boys-Exe@users.noreply.github.com>
2021-07-30 19:27:21 +0700

    Update config.py

--- config.py
+++ config.py
@@ -13,12 +13,12 @@ ARQ_API_KEY = getenv("ARQ_API_KEY", "QFOTZM-GSZUFY-CHGHRX-TDEHOZ-ARQ")
 BOT_USERNAME = getenv("BOT_USERNAME")
 ASSISTANT_NAME = getenv("ASSISTANT_NAME")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "GroupMusicRandom")
-OWNER = getenv("OWNER", "Tomi_Setiawan")
+OWNER = getenv("OWNER", "Tomi_sn")
 STICKER_ID = getenv("STICKER_ID", "CAACAgUAAxkBAAFF-Bdg-i8JvMgppo9DCVkFV9pPVSprzgACdwIAAsgM0VdqqiQQ6Hdw7CAE")
 START_IMAGE = getenv("START_IMAGE", "https://telegra.ph/file/3cf32d3eae4c81dca0792.jpg")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
 
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ - !").split())
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ . , - : ; !").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
