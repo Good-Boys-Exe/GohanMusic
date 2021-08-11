f0e34681c336d1b24938d57798925c27b984bd77
Good-Boys-Exe <87267285+Good-Boys-Exe@users.noreply.github.com>
2021-08-11 22:55:31 +0700

    Create asupan.py

---
+++ GohanMusic/asupan.py
@@ -0,0 +1,26 @@
+# 🍀 © @tofik_dn
+# ⚠️ Do not remove credits
+
+import requests
+from pyrogram import Client
+from config import BOT_USERNAME as bu
+from helpers.filters import command
+
+@Client.on_message(command(["asupan", f"asupan@{bu}"]))
+async def asupan(client, message):
+    try:
+        resp = requests.get("https://tede-api.herokuapp.com/api/asupan/ptl").json()
+        results = f"{resp['url']}"
+        return await client.send_video(message.chat.id, video=results, caption=f"@{resp['by']}")
+    except Exception:
+        await message.reply_text("`Something went wrong LOL...`")
+
+
+@Client.on_message(command(["wibu", f"wibu@{bu}"]))
+async def wibu(client, message):
+    try:
+        resp = requests.get("https://tede-api.herokuapp.com/api/asupan/wibu").json()
+        results = f"{resp['url']}"
+        return await client.send_video(message.chat.id, video=results, caption=f"@{resp['by']}")
+    except Exception:
+        await message.reply_text("`Something went wrong LOL...`")
