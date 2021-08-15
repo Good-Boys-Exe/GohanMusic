8f2d836b841a65941715360b0c83d5ccc7cfce06
TOMI SETIAWAN <87267285+Good-Boys-Exe@users.noreply.github.com>
2021-08-15 13:32:24 +0700

    Update private.py

--- GohanMusic/private.py
+++ GohanMusic/private.py
@@ -78,8 +78,7 @@ Perintah semua admin grup
 help_callback_filter = filters.create(lambda _, __, query: query.data.startswith('helps+'))
 
 @Client.on_callback_query(help_callback_filter)
-async def helps_(client: Client, message: Message):
-def help_answer(client, callback_query):
+def helps_answer(client, callback_query, message: Message):
     chat_id = callback_query.from_user.id
     disable_web_page_preview=True
     message_id = callback_query.message.message_id
