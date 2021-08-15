b01537d3edb586eeaf0efce976101e3daff69471
TOMI SETIAWAN <87267285+Good-Boys-Exe@users.noreply.github.com>
2021-08-15 14:29:49 +0700

    Update config.py

--- config.py
+++ config.py
@@ -22,4 +22,3 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ . , - : ; !").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
-
