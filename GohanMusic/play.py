1928bf00bb6ce5f408ae531bfa1601e75abe49ee
TOMI SETIAWAN <87267285+Good-Boys-Exe@users.noreply.github.com>
2021-08-15 16:15:29 +0700

    Update play.py

--- GohanMusic/play.py
+++ GohanMusic/play.py
@@ -15,7 +15,7 @@ from callsmusic.callsmusic import client as USER
 from pyrogram.errors import UserAlreadyParticipant
 from downloaders import youtube
 
-from config import que, DURATION_LIMIT, SUPPORT_GROUP, BOT_USERNAME as bu, BOT_NAME as bn, ARQ_API_KEY as aak, BOT_IMAGE as bi, OWNER
+from config import que, DURATION_LIMIT, SUPPORT_GROUP, BOT_USERNAME as bu, BOT_NAME as bn, ARQ_API_KEY as aak, BOT_IMAGE as bi
 from helpers.filters import command, other_filters
 from helpers.decorators import authorized_users_only
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
