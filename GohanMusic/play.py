4510cf7406a407b3813a0c88cf44b4871f8a9afc
TOMI SETIAWAN <87267285+Good-Boys-Exe@users.noreply.github.com>
2021-09-27 20:30:55 +0700

    Update play.py

--- GohanMusic/play.py
+++ GohanMusic/play.py
@@ -482,7 +482,7 @@ async def play(_, message: Message):
         query = toxt
         await lel.edit("**🎵 Memproses lagu...**")
         ydl_opts = {
-            "format": "bestaudio",
+            "format": "bestaudio/best",
         }
         try:
             results = YoutubeSearch(query, max_results=1).to_dict()
@@ -520,7 +520,7 @@ async def play(_, message: Message):
         print(query)
         await lel.edit("**🎵 Memproses lagu...**")
         ydl_opts = {
-            "format": "bestaudio",
+            "format": "bestaudio/best",
         }
 
         try:
@@ -842,7 +842,7 @@ async def ytplay(_, message: Message):
     print(query)
     await lel.edit("**🎵 Memproses lagu...**")
     ydl_opts = {
-        "format": "bestaudio",
+        "format": "bestaudio/best",
     }
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
