ef23ec5c63792d5d4c9de876c3e282bb6124b16c
TOMI SETIAWAN <87267285+Good-Boys-Exe@users.noreply.github.com>
2021-09-28 11:40:39 +0700

    Update msg.py

--- GohanMusic/msg.py
+++ GohanMusic/msg.py
@@ -23,7 +23,6 @@ class Messages:
         f"""
 <b>Perintah semua anggota grup
 • /play (judul lagu) - Untuk Memutar lagu yang Anda minta melalui YouTube
-• /ytplay (judul lagu) - Untuk Memutar lagu yang Anda minta melalui YouTube Tanpa Pilihan
 • /song (judul lagu) - Untuk Mendownload lagu dari YouTube
 • /vsong (judul video) - Untuk Mendownload Video di YouTube
 • /lyrics (judul lagu) Untuk Mencari Lirik Lagu
