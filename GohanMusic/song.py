0d463e91614ac5371ed0e603e26d6e6f0af8af1b
TOMI SETIAWAN <87267285+Good-Boys-Exe@users.noreply.github.com>
2021-09-30 12:42:09 +0700

    Update song.py

--- GohanMusic/song.py
+++ GohanMusic/song.py
@@ -23,7 +23,7 @@ from config import BOT_NAME, BOT_USERNAME, DURATION_LIMIT
 from helpers.filters import command
 
 
-@Client.on_message(command(["song", f"song@{BOT_USERNAME}"]))
+@Client.on_message(command(["music", f"music@{BOT_USERNAME}"]))
 async def ytmusic(client, message: Message):
     urlissed = get_text(message)
     if not urlissed:
@@ -101,7 +101,7 @@ async def ytmusic(client, message: Message):
             os.remove(files)
 
 
-@Client.on_message(command(["music", f"music@{BOT_USERNAME}"]) & ~filters.channel)
+@Client.on_message(command(["song", f"song@{BOT_USERNAME}"]) & ~filters.channel)
 def song(client, message):
     user_id = message.from_user.id
     user_name = message.from_user.first_name
@@ -406,6 +406,78 @@ async def ytmusic(client, message: Message):
     )
     await pablo.delete()
     is_downloading = False
+    for files in (sedlyf, file_stark):
+        if files and os.path.exists(files):
+            os.remove(files)
+
+
+@Client.on_message(command(["video", f"video@{BOT_USERNAME}"]))
+async def ytvideo(client, message: Message):
+    urlissed = get_text(message)
+
+    pablo = await client.send_message(
+        message.chat.id, f"`Mendapatkan {urlised} Dari Youtube Mohon Tunggu.`"
+    )
+    if not urlissed:
+        await pablo.edit(
+            "Sintaks Perintah Tidak Valid, Silakan Periksa Menu Bantuan Untuk Tahu Lebih Banyak!"
+        )
+        return
+
+    search = SearchVideos(f"{urlissed}", offset=1, mode="dict", max_results=1)
+    mi = search.result()
+    mio = mi["search_result"]
+    mo = mio[0]["link"]
+    thum = mio[0]["title"]
+    fridayz = mio[0]["id"]
+    mio[0]["channel"]
+    kekme = f"https://img.youtube.com/vi/{fridayz}/hqdefault.jpg"
+    await asyncio.sleep(0.6)
+    url = mo
+    sedlyf = wget.download(kekme)
+    opts = {
+        "format": "best",
+        "addmetadata": True,
+        "key": "FFmpegMetadata",
+        "prefer_ffmpeg": True,
+        "geo_bypass": True,
+        "nocheckcertificate": True,
+        "postprocessors": [{"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}],
+        "outtmpl": "%(id)s.mp4",
+        "logtostderr": False,
+        "quiet": True,
+    }
+    try:
+        with YoutubeDL(opts) as ytdl:
+            ytdl_data = ytdl.extract_info(url, download=True)
+    except Exception as e:
+        await event.edit(event, f"**Gagal Mengunduh** \n**Kesalahan :** `{str(e)}`")
+        return
+    c_time = time.time()
+    file_stark = f"{ytdl_data['id']}.mp4"
+    capy = f"""
+**🏷️ Nama Video:** [{thum}]({mo})
+**⏱️ Durasi Video:** {duration}
+**🤖 Diunggah Oleh:** [{BOT_NAME}](https://t.me/{BOT_USERNAME})
+**🎧 Permintaan Dari:** {message.from_user.mention}
+"""
+    await client.send_video(
+        message.chat.id,
+        video=open(file_stark, "rb"),
+        duration=int(ytdl_data["duration"]),
+        file_name=str(ytdl_data["title"]),
+        thumb=sedlyf,
+        caption=capy,
+        supports_streaming=True,
+        progress=progress,
+        progress_args=(
+            pablo,
+            c_time,
+            f"`Mendownload {urlissed} Dari YouTube!`",
+            file_stark,
+        ),
+    )
+    await pablo.delete()
     for files in (sedlyf, file_stark):
         if files and os.path.exists(files):
             os.remove(files)
