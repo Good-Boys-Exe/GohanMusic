43f204d647b6e63f19bc8e4ad6685af4e076bda6
TOMI SETIAWAN <87267285+Good-Boys-Exe@users.noreply.github.com>
2021-10-05 13:25:52 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 que = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
+SESSION_NAME = getenv("SESSION_NAME")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
+BG_IMAGE = getenv("BG_IMAGE")
 admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
@@ -15,10 +16,7 @@ BOT_USERNAME = getenv("BOT_USERNAME")
 ASSISTANT_NAME = getenv("ASSISTANT_NAME")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "GroupMusicRandom")
 OWNER = getenv("OWNER", "Tomi_sn")
-STICKER_ID = getenv(
-    "STICKER_ID",
-    "CAACAgUAAxkBAAFF-Bdg-i8JvMgppo9DCVkFV9pPVSprzgACdwIAAsgM0VdqqiQQ6Hdw7CAE",
-)
+
 BOT_IMAGE = getenv("BOT_IMAGE", "https://telegra.ph/file/3cf32d3eae4c81dca0792.jpg")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
